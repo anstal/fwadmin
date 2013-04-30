b3079c2f4d0c5795dade6beae504b3af4e50d955
Peter Golm <golm.peter@gmail.com>
2013-04-30 16:37:34 +0200

    fix: pep8 problems

--- fwadmin/context_processors.py
+++ fwadmin/context_processors.py
@@ -6,4 +6,4 @@ from django_project.settings import (
 def is_moderator(request):
     is_moderator = request.user.groups.filter(
         name=FWADMIN_MODERATORS_USER_GROUP).count()
-    return { 'is_moderator': is_moderator }
+    return {'is_moderator': is_moderator}
